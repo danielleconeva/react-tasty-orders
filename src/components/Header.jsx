e8ebaa081146a135ff2816914ac25bce6bd1ae4a
danielleconeva <dconewa@gmail.com>
2025-09-09 10:52:04 +0300

    add Cart context and reusable Modal component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,24 @@
-import logoImg from '../assets/logo.jpg';
+import { useContext } from 'react';
+
 import Button from './UI/Button.jsx';
+import logoImg from '../assets/logo.jpg';
+import CartContext from '../store/CartContext.jsx';
 
 export default function Header() {
+    const cartCtx = useContext(CartContext);
+
+    const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
+        return totalNumberOfItems + item.quantity;
+    }, 0);
+
     return (
         <header id="main-header">
             <div id="title">
                 <img src={logoImg} alt="A restaurant" />
-                <h1>Tasty Orders</h1>
+                <h1>ReactFood</h1>
             </div>
             <nav>
-                <Button textOnly>Cart (0)</Button>
+                <Button textOnly>Cart ({totalCartItems})</Button>
             </nav>
         </header>
     );
